7acb492eb132faa783eb57c0c0801e6fa5e8385a
Yuduspiti <yuduspiti@gmail.com>
2025-06-15 20:44:27 -0500

    Update TestAdsoController.java
    agrego palabra

--- Tienda-ModaColombia/src/test/java/controller/TestAdsoController.java
+++ Tienda-ModaColombia/src/test/java/controller/TestAdsoController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestAdsoController {
 
     public String holaMundoAdso(){
-        return "Hola esto es un saludo desde un WS - SpringBoot";
+        return "Hola esto es un saludo desde un WS - SpringBoot  yuly";
     }
 }
